a62030fc2d28f557b2760d9da2e6db4fd8118b3d
killxistcool <141137008+killxistcool@users.noreply.github.com>
2025-07-30 02:57:55 +0200

    Update hydrogen-installer.sh

--- hydrogen-installer.sh
+++ hydrogen-installer.sh
@@ -10,33 +10,63 @@ echo "     Hydrogen Helper"
 echo "       by Flawless"
 echo "============================"
 echo
-echo "1) Install Hydrogen"
-echo "2) Uninstall Hydrogen"
-echo "3) Reinstall Hydrogen"
+echo "What are you looking for?"
+echo
+echo "1) Hydrogen Information"
 echo "0) Exit"
 echo
 read -p "Choose an option: " choice
 
 case $choice in
     1)
-        echo "Installing Hydrogen..."
-        sudo bash -c "$(curl -fsSL https://www.hydrogen.lat/install)"
-        ;;
-    2)
-        echo "Uninstalling Hydrogen..."
-        sudo rm -rf ~/Applications/Hydrogen-M.app
-        echo "Hydrogen has been uninstalled."
-        ;;
-    3)
-        echo "Reinstalling Hydrogen..."
-        sudo rm -rf ~/Hydrogen
-        sudo bash -c "$(curl -fsSL https://www.hydrogen.lat/install)"
+        clear
+        echo "============================"
+        echo "   Hydrogen Information"
+        echo "============================"
+        echo
+        echo "1) FAQ"
+        echo "2) sUNC"
+        echo "3) Status"
+        echo "4) Key Prices"
+        echo "5) Key Tutorial"
+        echo "0) Back"
+        echo
+        read -p "Choose an option: " infoChoice
+        case $infoChoice in
+            1)
+                open "https://hydrostuff.github.io/Hydrogen-Knowledgebase/"
+                ;;
+            2)
+                echo "**Hydrogen is at 94% sUNC.**"
+                echo "Missing: **\`replicatesignal\`**, and **\`getcallbackvalue\`**"
+                echo "Test link: https://r.sunc.su/iVvotxiPoV"
+                ;;
+            3)
+                echo "Hydrogen Status: 🟢 UP"
+                ;;
+            4)
+                echo "Hydrogen Key Prices:"
+                echo "• 1 Week: \$3 (❌ Sold Out)"
+                echo "• 2 Weeks: \$5.00 (❌ Sold Out)"
+                echo "• 1 Month: \$8.00 (✅ Available)"
+                echo "• 3 Months: \$18.00 (✅ Available)"
+                ;;
+            5)
+                open "https://youtu.be/sq8rHrBykJE"
+                ;;
+            0)
+                exec "$0"
+                ;;
+            *)
+                echo "Invalid option."
+                ;;
+        esac
         ;;
     0)
         echo "Adios"
         exit 0
         ;;
     *)
-        echo "Invalid Option."
+        echo "Invalid option."
         ;;
 esac
